0500e49d6ed329f7b616b310557f6ff6805a2241
MaryInda <163969353+MaryInda@users.noreply.github.com>
2024-04-24 18:35:59 -0700

    Add files via upload

---
+++ JuegoDeLosSeisesP4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.labpoo.juegodelosseisesp4;
+
+/**
+ *
+ * @author Usuario
+ */
+public class JuegoDeLosSeisesP4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
